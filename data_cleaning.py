d04a7645f83f006b26a7ac88a6bc7e0938a2a7b5
Anthony Fort <ajf2195@columbia.edu>
2023-12-04 15:05:43 +0000

    Starts data cleaning function

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,1 +1,27 @@
+import pandas as pd
+import data_extraction
+
+link = 'https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf'
+
+df_from_extraction = data_extraction.new_df.retrieve_pdf_data(link)
+
 class DataCleaning:
+  def clean_card_data(self, df):
+    df.replace(to_replace = 'NULL', value=None, inplace=True)
+    df['date_payment_confirmed'] = pd.to_datetime(df['date_payment_confirmed'], errors='coerce')
+    df['expiry_date'] = pd.to_datetime(df['expiry_date'], format='%m/%d', errors='coerce')
+    df.dropna(inplace=True, ignore_index=True)
+    df['expiry_date'] = df['expiry_date'].astype(str).str[5:]
+    df['card_number'] = df['card_number'].astype(str).str.strip().str.upper()
+    df['card_provider'] = df['card_provider'].str.strip().str.upper()
+    print(df.isnull().values.any())
+    print(df.isnull().sum())
+    print(df['card_number'].head(20))
+    
+    # print(df['expiry_date'].head(10))
+    # print(df.describe())
+    # df.info()
+
+df_to_clean = DataCleaning()
+
+df_to_clean.clean_card_data(df_from_extraction)
--- data_extraction.py
+++ data_extraction.py
@@ -2,14 +2,11 @@ import tabula
 import pandas as pd
 from functools import reduce
 
-link = 'https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf'
-
 class DataExtractor():
 
   def retrieve_pdf_data(self, link):
     dfs = tabula.read_pdf(link, pages='all') # Returns list of dataframes
     df = reduce(lambda left, right: pd.concat((left, right), axis=0, join='outer', ignore_index=True), dfs) # Joins dataframes into single dataframe
-    print(df.tail(20))
+    return df
 
 new_df = DataExtractor()
-new_df.retrieve_pdf_data(link)
