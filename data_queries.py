b7bcb73fe3bf9483f5a98065f31446dd8841b8aa
Anthony Fort <ajf2195@columbia.edu>
2023-12-14 10:29:09 +0000

    Converts dtypes in dim_users

--- data_queries.py
+++ data_queries.py
@@ -13,6 +13,13 @@ with engine.execution_options(isolation_level='AUTOCOMMIT').connect() as connect
   connection.execute(text("ALTER TABLE orders_table ALTER store_code TYPE varchar(12) USING store_code::varchar(12);"))
   connection.execute(text("ALTER TABLE orders_table ALTER product_code TYPE varchar(11) USING card_number::varchar(11);"))
   connection.execute(text("ALTER TABLE orders_table ALTER product_quantity TYPE smallint USING product_quantity::smallint;"))
+  connection.execute(text("ALTER TABLE dim_users ALTER first_name TYPE varchar(255) USING first_name::varchar(255);"))
+  connection.execute(text("ALTER TABLE dim_users ALTER last_name TYPE varchar(255) USING last_name::varchar(255);"))
+  connection.execute(text("ALTER TABLE dim_users ALTER date_of_birth TYPE date USING date_of_birth::date;"))
+  connection.execute(text("ALTER TABLE dim_users ALTER country_code TYPE varchar(2) USING country_code::varchar(2);"))
+  connection.execute(text("ALTER TABLE dim_users ALTER user_uuid TYPE uuid USING user_uuid::uuid;"))
+  connection.execute(text("ALTER TABLE dim_users ALTER join_date TYPE date USING join_date::date;"))
+
 
   
 
