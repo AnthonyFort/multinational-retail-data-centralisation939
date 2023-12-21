aa9eb5f69d2e247606e6e365c6da839fc5cc504c
Anthony Fort <ajf2195@columbia.edu>
2023-12-21 20:17:33 +0000

    Refactors code and reuploads database

--- data_extraction.py
+++ data_extraction.py
@@ -1,41 +1,57 @@
-import boto3
-import data_utils
-import json
-import os
 import pandas as pd
-import requests
 import tabula
+import boto3
+import os
+import requests
+import json
 
-from dotenv import find_dotenv, load_dotenv
 from functools import reduce
+from dotenv import find_dotenv, load_dotenv
 
-load_dotenv(find_dotenv())
+from data_utils import DatabaseConnector
+
 s3 = boto3.client('s3')
 
 api_key = os.environ.get('x-api-key')
-filename_for_downloaded_csv = os.environ.get('filename_for_downloaded_csv')
-sales_url = 'https://data-handling-public.s3.eu-west-1.amazonaws.com/date_details.json'
-store_endpoint =  'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/'
-store_numbers_url = ' https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores'
 
 headers = {
   'Content-Type': 'application/json',
   'x-api-key': api_key
 }
 
+card_details_link = os.environ.get('card_details_link')
+store_endpoint = os.environ.get('store_endpoint')
+store_numbers_url = os.environ.get('store_numbers_url')
+filename_for_downloaded_csv = os.environ.get('filename_for_downloaded_csv')
+filename_for_dim_date_times = os.environ.get('filename_for_dim_date_times')
+sales_url = os.environ.get('sales_url')
+
 class DataExtractor():
 
+  def __init__(self, db_connector):
+    self.db_connector = db_connector
+
+  def read_rds_table(self, table_name):
+    try:
+      with self.db_connector.engine.connect() as conn:
+        return pd.read_sql_table(table_name, conn)
+    except Exception as e:
+      print(e)  
+
   def retrieve_pdf_data(self, link):
-    dfs = tabula.read_pdf(link, pages='all') # Returns list of dataframes
-    df = reduce(lambda left, right: pd.concat((left, right), axis=0, join='outer', ignore_index=True), dfs) # Joins dataframes into single dataframe
-    return df
-  
+    try:
+      dfs = tabula.read_pdf(link, pages='all') # Returns list of dataframes
+      df = reduce(lambda left, right: pd.concat((left, right), axis=0, join='outer', ignore_index=True), dfs) # Joins dataframes into single dataframe
+      return df    
+    except Exception as e:
+      print(e)  
+
   def list_number_of_stores(self, endpoint, header_dict):
     response = requests.get(endpoint, headers=header_dict)
     data = response.json()
     number_of_stores = data['number_stores']
-    self.retreive_stores_data(number_of_stores, store_endpoint=store_endpoint)
-
+    return number_of_stores   
+  
   def retreive_stores_data(self, number_of_stores, store_endpoint):
     json_responses = []
     for n in range( number_of_stores):
@@ -45,20 +61,15 @@ class DataExtractor():
     with open('stores_data.json', 'w') as f:
       json.dump(json_responses, f)  
 
-  def extract_from_s3(self, bucket, key):
-    s3.download_file(bucket, key, filename_for_downloaded_csv)
-
-  
-  def read_rds_table(self, connector, table_name):
-    engine = connector.read_db_creds('db_creds.yaml')
-    return pd.read_sql_table(table_name, engine)
-  
   def extract_from_s3(self, bucket, key, filename):
-    s3.download_file(bucket, key, filename)
+    try:
+      s3.download_file(bucket, key, filename)    
+    except Exception as e:
+      print(e)  
 
-  def convert_csv_to_df(self):
-    df = pd.read_csv('products.csv')
-    return df
+  def convert_csv_to_df(self, filename):
+    df = pd.read_csv(filename)
+    return df   
 
   def extract_json(self, url):
     try:
@@ -70,24 +81,115 @@ class DataExtractor():
       else:
         print(response.status_code)  
     except Exception as e:
-      print(e)
+      print(e) 
 
-new_data_extractor = DataExtractor()
+db_connector = DatabaseConnector()
+db_connector.read_db_creds('db_creds.yaml')
+db_connector.init_db_engine()
 
-users_df = new_data_extractor.read_rds_table(data_utils.new_db_connector, 'legacy_users')
-orders_df = new_data_extractor.read_rds_table(data_utils.new_db_connector, 'orders_table')
-new_data_extractor.extract_json(sales_url)
+data_extractor = DataExtractor(db_connector)
+user_data_df = data_extractor.read_rds_table('legacy_users')
+card_data_df = data_extractor.retrieve_pdf_data(card_details_link)
+products_df = data_extractor.convert_csv_to_df('products.csv')
+orders_df = data_extractor.read_rds_table('orders_table')
+
+# number_of_stores = data_extractor.list_number_of_stores(store_numbers_url, headers)
+# data_extractor.retreive_stores_data(number_of_stores, store_endpoint)
+# products_df = data_extractor.extract_from_s3('data-handling-public', 'products.csv')
+# data_extractor.extract_json(sales_url)
 
 
 
-# new_data_extractor.read_rds_data(data_utils.new_db_connector.read_db_creds('db_creds.yaml', 'legacy_store_details'))
-# new_data_extractor.read_rds_data(data_utils.new_db_connector.read_db_creds('db_creds.yaml', 'orders_table'))
+# import boto3
+# import data_utils
+# import json
+# import os
+# import pandas as pd
+# import requests
+# import tabula
+
+# from dotenv import find_dotenv, load_dotenv
+# from functools import reduce
+
+# load_dotenv(find_dotenv())
+# s3 = boto3.client('s3')
+
+# api_key = os.environ.get('x-api-key')
+# filename_for_downloaded_csv = os.environ.get('filename_for_downloaded_csv')
+# sales_url = 'https://data-handling-public.s3.eu-west-1.amazonaws.com/date_details.json'
+# store_endpoint =  'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/'
+# store_numbers_url = ' https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores'
+
+# headers = {
+#   'Content-Type': 'application/json',
+#   'x-api-key': api_key
+# }
+
+# class DataExtractor():
+
+#   def retrieve_pdf_data(self, link):
+#     dfs = tabula.read_pdf(link, pages='all') # Returns list of dataframes
+#     df = reduce(lambda left, right: pd.concat((left, right), axis=0, join='outer', ignore_index=True), dfs) # Joins dataframes into single dataframe
+#     return df
+  
+#   def list_number_of_stores(self, endpoint, header_dict):
+#     response = requests.get(endpoint, headers=header_dict)
+#     data = response.json()
+#     number_of_stores = data['number_stores']
+#     self.retreive_stores_data(number_of_stores, store_endpoint=store_endpoint)
+
+#   def retreive_stores_data(self, number_of_stores, store_endpoint):
+#     json_responses = []
+#     for n in range( number_of_stores):
+#       response = requests.get(f'{store_endpoint}{n}', headers=headers)
+#       data = json.loads(response.text)
+#       json_responses.append(data)
+#     with open('stores_data.json', 'w') as f:
+#       json.dump(json_responses, f)  
+
+#   def extract_from_s3(self, bucket, key):
+#     s3.download_file(bucket, key, filename_for_downloaded_csv)
+
+  
+#   def read_rds_table(self, connector, table_name):
+#     engine = connector.read_db_creds('db_creds.yaml')
+#     return pd.read_sql_table(table_name, engine)
+  
+#   def extract_from_s3(self, bucket, key, filename):
+#     s3.download_file(bucket, key, filename)
+
+#   def convert_csv_to_df(self):
+#     df = pd.read_csv('products.csv')
+#     return df
+
+#   def extract_json(self, url):
+#     try:
+#       response = requests.get(url)
+#       if response.status_code == 200:
+#         data = response.json()
+#         with open('dim_date_times', 'w') as f:
+#           json.dump(data, f)
+#       else:
+#         print(response.status_code)  
+#     except Exception as e:
+#       print(e)
+
+# new_data_extractor = DataExtractor()
+
+# users_df = new_data_extractor.read_rds_table(data_utils.new_db_connector, 'legacy_users')
+# orders_df = new_data_extractor.read_rds_table(data_utils.new_db_connector, 'orders_table')
+# new_data_extractor.extract_json(sales_url)
 
 
-# new_df = DataExtractor()
 
-# # new_df.list_number_of_stores(store_numbers_url, headers)
+# # new_data_extractor.read_rds_data(data_utils.new_db_connector.read_db_creds('db_creds.yaml', 'legacy_store_details'))
+# # new_data_extractor.read_rds_data(data_utils.new_db_connector.read_db_creds('db_creds.yaml', 'orders_table'))
 
-# # new_df.extract_from_s3('data-handling-public', 'products.csv')
 
-# new_df.convert_csv_to_df()
+# # new_df = DataExtractor()
+
+# # # new_df.list_number_of_stores(store_numbers_url, headers)
+
+# # # new_df.extract_from_s3('data-handling-public', 'products.csv')
+
+# # new_df.convert_csv_to_df()
--- data_utils.py
+++ data_utils.py
@@ -1,8 +1,8 @@
-import os
 import yaml
-from dotenv import find_dotenv, load_dotenv
 from sqlalchemy import create_engine, inspect
 
+from dotenv import find_dotenv, load_dotenv
+import os
 load_dotenv(find_dotenv())
 
 DATABASE_TYPE = os.environ.get('DATABASE_TYPE')
@@ -14,34 +14,97 @@ PASSWORD = os.environ.get('PASSWORD')
 DATABASE = os.environ.get('DATABASE')
 PORT = 5432
 
-upload_engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
-upload_engine.execution_options(isolation_level='AUTOCOMMIT').connect()
+engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
+engine.execution_options(isolation_level='AUTOCOMMIT').connect()
 
-
-class DatabaseConnector:
+class DatabaseConnector():
 
   def __init__(self):
-      self.engine = None
-  
+    self.creds_dict = None
+    self.engine = None
+    self.upload_engine = None
+
   def read_db_creds(self, creds):
-    with open(creds, 'r') as f:
-      creds_dict = yaml.safe_load(f)
-      return self.init_db_engine(creds_dict)
+    try:
+      with open(creds, 'r') as f:
+        self.creds_dict = yaml.safe_load(f)
+    except Exception as e:
+      print(e)    
+      
+  def init_db_engine(self):
+    try:
+      self.engine = create_engine(f"postgresql://{self.creds_dict['RDS_USER']}:{self.creds_dict['RDS_PASSWORD']}@{self.creds_dict['RDS_HOST']}:{self.creds_dict['RDS_PORT']}/{self.creds_dict['RDS_DATABASE']}")
+    except Exception as e:
+      print(e)
 
-  def init_db_engine(self, creds):
-    self.engine = create_engine(f"postgresql://{creds['RDS_USER']}:{creds['RDS_PASSWORD']}@{creds['RDS_HOST']}:{creds['RDS_PORT']}/{creds['RDS_DATABASE']}")
-    self.engine.execution_options(isolation_level='AUTOCOMMIT').connect()
-    self.list_db_tables(self.engine)
-    return self.engine
+  def init_upload_engine(self):
+    try:
+      self.upload_engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
+    except Exception as e:
+      print(e)      
+  
+  def list_db_tables(self):
+    try:
+      with self.engine.connect() as conn:
+        inspection = inspect(conn)
+        return inspection.get_table_names()
+    except Exception as e:
+      print(e)  
 
-  def list_db_tables(self, engine):
-    inspection = inspect(engine)
-    print(inspection.get_table_names())
-    return engine
-    
   def upload_to_db(self, df, table_name):
-    df.to_sql(table_name, upload_engine, if_exists='replace') 
+    try:
+      with self.upload_engine.execution_options(isolation_level='AUTOCOMMIT').connect() as conn:
+        df.to_sql(table_name, conn, if_exists='replace')   
+    except Exception as e:
+      print(e)
+        
 
-new_db_connector = DatabaseConnector()
 
-# new_db_connector.read_db_creds('db_creds.yaml')
+
+# import os
+# import yaml
+# from dotenv import find_dotenv, load_dotenv
+# from sqlalchemy import create_engine, inspect
+
+# load_dotenv(find_dotenv())
+
+# DATABASE_TYPE = os.environ.get('DATABASE_TYPE')
+# DBAPI = os.environ.get('DBAPI')
+# HOST = os.environ.get('HOST')
+# # USER = os.environ.get('USER')
+# USER = 'postgres'
+# PASSWORD = os.environ.get('PASSWORD')
+# DATABASE = os.environ.get('DATABASE')
+# PORT = 5432
+
+# upload_engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
+# upload_engine.execution_options(isolation_level='AUTOCOMMIT').connect()
+
+
+# class DatabaseConnector:
+
+#   def __init__(self):
+#       self.engine = None
+  
+#   def read_db_creds(self, creds):
+#     with open(creds, 'r') as f:
+#       creds_dict = yaml.safe_load(f)
+#       return self.init_db_engine(creds_dict)
+
+#   def init_db_engine(self, creds):
+#     self.engine = create_engine(f"postgresql://{creds['RDS_USER']}:{creds['RDS_PASSWORD']}@{creds['RDS_HOST']}:{creds['RDS_PORT']}/{creds['RDS_DATABASE']}")
+#     self.engine.execution_options(isolation_level='AUTOCOMMIT').connect()
+#     self.list_db_tables(self.engine)
+#     return self.engine
+
+#   def list_db_tables(self, engine):
+#     inspection = inspect(engine)
+#     print(inspection.get_table_names())
+#     return engine
+    
+#   def upload_to_db(self, df, table_name):
+#     df.to_sql(table_name, upload_engine, if_exists='replace') 
+
+# new_db_connector = DatabaseConnector()
+
+# # new_db_connector.read_db_creds('db_creds.yaml')
